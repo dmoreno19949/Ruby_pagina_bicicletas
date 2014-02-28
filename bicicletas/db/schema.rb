f4e568966a0192d71e52a817519e8907ef94939c
dmoreno19949 <el_david_mo@hotmail.com>
2014-02-28 17:25:32 +0100

    Paperclip brand y bike Correcto

--- bicicletas/db/schema.rb
+++ bicicletas/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140228135915) do
+ActiveRecord::Schema.define(version: 20140228161255) do
 
   create_table "bikes", force: true do |t|
     t.string   "name"
@@ -31,6 +31,10 @@ ActiveRecord::Schema.define(version: 20140228135915) do
     t.string   "localizacion"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "logo_file_name"
+    t.string   "logo_content_type"
+    t.integer  "logo_file_size"
+    t.datetime "logo_updated_at"
   end
 
 end
